8f4b96985e15011bebe61563b1bf7c90cd1c2eb8
pikachu0310 <pikachu13711@gmail.com>
2023-12-17 23:28:46 +0900

    GetKemonoForBattleByOwnerIdにLIMIT1を追加して高速化

--- internal/repository/kemono.go
+++ internal/repository/kemono.go
@@ -128,7 +128,7 @@ func (r *Repository) GetMyKemonoByUserId(ctx context.Context, ownerID uuid.UUID)
 
 func (r *Repository) GetKemonoForBattleByOwnerId(ctx context.Context, ownerID uuid.UUID) (*domains.Kemono, error) {
 	var kemono domains.Kemono
-	if err := r.db.GetContext(ctx, &kemono, "SELECT * FROM kemono WHERE owner_id = ? AND is_for_battle = TRUE AND has_child = FALSE", ownerID); err != nil {
+	if err := r.db.GetContext(ctx, &kemono, "SELECT * FROM kemono WHERE owner_id = ? AND is_for_battle = TRUE AND has_child = FALSE LIMIT 1", ownerID); err != nil {
 		return nil, fmt.Errorf("select kemono: %w", err)
 	}
 
